a3a92e95ae7972b5bd0111b0238cd4f57663344f
ttminh81 <minhtrinhtu81@gmail.com>
2025-06-11 17:35:11 +0700

    function test

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-printf("\nA sample C program\n\n");
-return 0;
+int main(){
+    //Cấp phát vùng nhớ tương đương mảng 100 phần tử int
+    //sizeof(int) = 4
+    int *a = (int*)malloc(100 * sizeof(int));
+    //Cấp phát vùng nhớ tương đương mảng 1000 phần tử char
+    //sizeof(char) = 1
+    char *c = (char*)malloc(1000 * sizeof(char));
+    return 0;
 }
